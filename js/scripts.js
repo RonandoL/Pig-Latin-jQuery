bdb8c3bcbe330c6f4587ed0fe663c2af085f76e8
Ronando Long <ronando.long@gmail.com>
2016-03-22 18:11:10 -0700

    Initial commit, files created

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+// Business Logic
+// var helloWorld = function(){
+//   return false;
+//
+//   For loops and shit go here
+//   If/else like a madman here
+//
+//  return shit like a mo-fo here
+//
+//   ...whatchyu lookin at bitch? Green those specs and git back to work!
+//  Time fo yo Interface Logic, then suckface when it's all green.
+// };
+
+
+
+
+
+
+
+
+// User Interface Logic
+// $(document).ready(function() {
+//   $("form#IDselector").submit(function(event) {
+//     // variables and shit go here, bitch!
+//
+//     // link your muthafuckin' variables to yo bad-ass bidness logic function, Yo!
+//
+//     // Make shit happen here...BITCH!
+//
+//     // Muthafuckin' variables connect to your output down here hommie.
+//
+//     // Show me some money, hunny, and reveal that hiddin fine shit you got stashed.
+//
+//     // It's all good baby.'
+//     event.preventDefault();
+//   });
+// });
+
+// Jumbotron background image - goes inside UI Logic
+// var jumboHeight = $('.jumbotron').outerHeight();
+//   function parallax(){
+//       var scrolled = $(window).scrollTop();
+//       $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+//   }
+//
+//   $(window).scroll(function(e){
+//       parallax();
+//   });
